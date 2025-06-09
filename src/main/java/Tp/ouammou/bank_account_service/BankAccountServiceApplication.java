f53ec2e96d3934570d731bcd08f9949379c13f83
Mourad Ouammou <mouradouammou8@gmail.com>
2025-06-09 12:12:06 +0100

    creation des comptes pour le test

--- src/main/java/Tp/ouammou/bank_account_service/BankAccountServiceApplication.java
+++ src/main/java/Tp/ouammou/bank_account_service/BankAccountServiceApplication.java
@@ -1,7 +1,13 @@
 package Tp.ouammou.bank_account_service;
 
+import Tp.ouammou.bank_account_service.entites.BankAccount;
+import Tp.ouammou.bank_account_service.repositories.BankAccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class BankAccountServiceApplication {
@@ -10,4 +16,21 @@ public class BankAccountServiceApplication {
 		SpringApplication.run(BankAccountServiceApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner start(BankAccountRepository bankAccountRepository) {
+		return args -> {
+			for(int i=0;i<10;i++) {
+				bankAccountRepository.save(
+						BankAccount.builder()
+								.Id("BA" + i)
+								.creationDate(new Date())
+								.balance(1000 + i * 100)
+								.accountType(i % 2 == 0 ? "CURRENT_ACCOUNT" : "SAVINGS_ACCOUNT")
+								.currency("USD")
+								.build()
+				);
+			}
+		};
+	}
 }
