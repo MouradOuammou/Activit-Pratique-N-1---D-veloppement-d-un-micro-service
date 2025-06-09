e60ee8d39af3fdcedcb2dd2cd91e0897e93a1139
Mourad Ouammou <mouradouammou8@gmail.com>
2025-06-09 14:37:38 +0100

    creation de projection

--- src/main/java/Tp/ouammou/bank_account_service/repositories/BankAccountRepository.java
+++ src/main/java/Tp/ouammou/bank_account_service/repositories/BankAccountRepository.java
@@ -2,7 +2,9 @@ package Tp.ouammou.bank_account_service.repositories;
 
 import Tp.ouammou.bank_account_service.entites.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
 
 }
